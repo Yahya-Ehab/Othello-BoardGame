46bfc0189cc0893d097d8aecace4d523232152c8
Dark <mohamed2003elashmawy@gmail.com>
2024-05-14 04:35:12 +0300

    updated sprite names and added a pointer to show last piece placed

--- GUI.py
+++ GUI.py
@@ -13,16 +13,21 @@ screen = pygame.display.set_mode((WINDOW_SIZE, WINDOW_SIZE))
 pygame.display.set_caption("Othello")
 
 # Load tile sprites
-empty_tile_unscaled = pygame.image.load("Images/Empty Tile.png")
+empty_tile_unscaled = pygame.image.load("Images/Tile (Empty).png")
 black_tile_unscaled = pygame.image.load("Images/Black Tile.png")
 white_tile_unscaled = pygame.image.load("Images/White Tile.png")
 open_tile_unscaled = pygame.image.load("Images/Open Tile.png")
-hover_tile_unscaled = pygame.image.load("Images/Hover Tile.png")
-hover_white_tile_unscaled = pygame.image.load("Images/Hover White Tile.png")
-hover_black_tile_unscaled = pygame.image.load("Images/Hover Black Tile.png")
-hover_open_tile_unscaled = pygame.image.load("Images/Hover Open Tile.png")
-paused_white_tile_unscaled = pygame.image.load("Images/Paused White Tile.png")
-paused_black_tile_unscaled = pygame.image.load("Images/Paused Black Tile.png")
+hover_tile_unscaled = pygame.image.load("Images/Tile (Hover).png")
+hover_black_tile_unscaled = pygame.image.load("Images/Black Tile (Hover).png")
+hover_white_tile_unscaled = pygame.image.load("Images/White Tile (Hover).png")
+hover_open_tile_unscaled = pygame.image.load("Images/Open Tile (Hover).png")
+paused_black_tile_unscaled = pygame.image.load("Images/Black Tile (Paused).png")
+paused_white_tile_unscaled = pygame.image.load("Images/White Tile (Paused).png")
+current_black_unscaled = pygame.image.load("Images/Current Black Tile.png")
+current_white_tile_unscaled = pygame.image.load("Images/Current White Tile.png")
+hover_current_black_unscaled = pygame.image.load("Images/Current Black Tile (Hover).png")
+hover_current_white_unscaled = pygame.image.load("Images/Current White Tile (Hover).png")
+
 
 # Setup tiles
 tile_size = int(WINDOW_SIZE / 8)
@@ -31,17 +36,25 @@ black_tile = pygame.transform.scale(black_tile_unscaled, (tile_size, tile_size))
 white_tile = pygame.transform.scale(white_tile_unscaled, (tile_size, tile_size))
 open_tile = pygame.transform.scale(open_tile_unscaled, (tile_size, tile_size))
 hover_tile = pygame.transform.scale(hover_tile_unscaled, (tile_size, tile_size))
-hover_white_tile = pygame.transform.scale(hover_white_tile_unscaled, (tile_size, tile_size))
 hover_black_tile = pygame.transform.scale(hover_black_tile_unscaled, (tile_size, tile_size))
+hover_white_tile = pygame.transform.scale(hover_white_tile_unscaled, (tile_size, tile_size))
 hover_open_tile = pygame.transform.scale(hover_open_tile_unscaled, (tile_size, tile_size))
-paused_white_tile = pygame.transform.scale(paused_white_tile_unscaled, (tile_size, tile_size))
 paused_black_tile = pygame.transform.scale(paused_black_tile_unscaled, (tile_size, tile_size))
+paused_white_tile = pygame.transform.scale(paused_white_tile_unscaled, (tile_size, tile_size))
+current_black_tile = pygame.transform.scale(current_black_unscaled, (tile_size, tile_size))
+current_white_tile = pygame.transform.scale(current_white_tile_unscaled, (tile_size, tile_size))
+hover_current_black_tile = pygame.transform.scale(hover_current_black_unscaled, (tile_size, tile_size))
+hover_current_white_tile = pygame.transform.scale(hover_current_white_unscaled, (tile_size, tile_size))
+
+
 board = Board().board
 othello = Othello()
 
 # Piece sound
 move_sound = pygame.mixer.Sound("Sounds/Piece_Sound.wav")
 
+lastX = -1
+lastY = -1
 
 # NOTE: the (x, y) points are reversed in pygame, where (x) is columns and (y) is rows
 # x increases going right
@@ -56,7 +69,11 @@ def draw_board(boardc):
     for i in range(8):
         for j in range(8):
             # For hover
-            if i == x and j == y and board[i][j] == 0:
+            if i == x and j == y and x == lastX and y == lastY and board[i][j] == 1:
+                screen.blit(hover_current_black_tile, (i * tile_size, j * tile_size))
+            elif i == x and j == y and x == lastX and y == lastY and board[i][j] == 2:
+                screen.blit(hover_current_white_tile, (i * tile_size, j * tile_size))
+            elif i == x and j == y and board[i][j] == 0:
                 screen.blit(hover_tile, (i * tile_size, j * tile_size))
             elif i == x and j == y and board[i][j] == 1:
                 screen.blit(hover_black_tile, (i * tile_size, j * tile_size))
@@ -66,6 +83,10 @@ def draw_board(boardc):
                 screen.blit(hover_open_tile, (i * tile_size, j * tile_size))
             
             # For normal tiles
+            elif i == lastX and j == lastY and board[i][j] == 1:
+                screen.blit(current_black_tile, (i * tile_size, j * tile_size))
+            elif i == lastX and j == lastY and board[i][j] == 2:
+                screen.blit(current_white_tile, (i * tile_size, j * tile_size))
             elif board[i][j] == 0:
                 screen.blit(empty_tile, (i * tile_size, j * tile_size))
             elif board[i][j] == 1:
@@ -74,6 +95,8 @@ def draw_board(boardc):
                 screen.blit(white_tile, (i * tile_size, j * tile_size))
             elif board[i][j] == 3:
                 screen.blit(open_tile, (i * tile_size, j * tile_size))
+    pygame.display.update()
+
 
 # This checks if there are available moves for the current player, otherwise it skips their turn
 # FIXME: this crashes the game at the end because there are no moves to be made, so we need an end screen
@@ -101,8 +124,6 @@ def skip_turn(board, x, y):
 screen.fill((255, 255, 255))
 running = True
 turn = 1
-prevX = -1
-prevY = -1
 
 # Main loop
 while running:
@@ -115,7 +136,7 @@ while running:
         othello.check_open_moves(board, turn)
         draw_board(board)
 
-        if prevX != -1 and prevY != -1 and skip_turn(board, x, y):
+        if lastX != -1 and lastY != -1 and skip_turn(board, x, y):
             turn = 2 if turn == 1 else 1
             continue
 
@@ -132,8 +153,8 @@ while running:
             elif turn == 2:
                 board[x][y] = 2
             
-            prevX = x
-            prevY = y
+            lastX = x
+            lastY = y
             
             othello.update_pieces(board, turn, x, y, "", True)
             move_sound.play()
